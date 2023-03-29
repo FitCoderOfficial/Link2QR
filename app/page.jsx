67452e93e98666d70915442cb637d20f11341479
ShinSaebyeok <toqur1219@naver.com>
2023-03-29 18:39:29 +0900

    feat:home design title

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>page</div>
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        Just Do it
+        <br className='max-md:hidden' />
+        <span className='green_gradient'>Your Personal Guide to Wellness!</span>
+      </h1>
+      <p className='desc text-center'>
+        Green Fit Ai is an open-source project that aims to help people live a healthier life.
+      </p>
+    </section>
   )
 }
 
