033db2b2cb3b3d44e26b422cf86521a25b6fe387
ShinSaebyeok <toqur1219@naver.com>
2023-05-01 18:46:41 +0900

    feat: globe icons

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,8 +1,31 @@
 "use client"
 
 import { Country, City} from "country-state-city"
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import Select from "react-select"
+import { GlobeIcon } from "@heroicons/react/solid"
 
+
+type option = {
+  value: {
+    latetude: string,
+    longitude: string,
+    isoCode: string,
+  },
+  label: string,
+} | null;
+
+type cityOption = {
+  value: {
+    latetude: string,
+    longitude: string,
+    country: string,
+    name: string,
+    stateCode: string,
+  },
+  label: string,
+} | null;
 
 const options = Country.getAllCountries().map((country) => ({
   value: {
@@ -16,9 +39,25 @@ const options = Country.getAllCountries().map((country) => ({
 
 
 function CityPicker() {
+  const [selectedCountry, setSelectedCountry] = useState<option>(null);
+  const [selectedCity, setSelectedCity] = useState<cityOption>(null);
+  const router = useRouter();
+
+  const handleSelectedCountry = (option: option) => {
+    setSelectedCountry(option);
+    setSelectedCity(null);
+  }
   return (
     <div>
-      <Select options={options} />
+      <div className="flex items-center space-x-2 text-white/80">
+        <GlobeIcon className="h-5 w-5 text-white" />
+        <label htmlFor="country">Country</label>
+      </div>
+      <Select 
+      className="text-black"z
+      value={selectedCountry}
+      onChange={handleSelectedCountry}
+      options={options} />
     </div>
   )
 }
