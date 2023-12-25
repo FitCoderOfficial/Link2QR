dfa8cbbaed73bf8cc0a514df59c9759035c60317
Shin Saebyeok <toqur1219@naver.com>
2023-12-25 16:29:23 +0900

    feat: adding nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,8 +1,27 @@
+'use client'
 
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav className='flex-between w-full mb-16 pt-3'>
+      <Link href='/' className='flex gap-2 flex-center'>
+        <Image
+          src='/assets/images/logo.svg'
+          alt='logo'
+          width={30}
+          height={30}
+          className='object-contain'
+        />
+        <p className='logo_text'>My QR</p>
+      </Link>
+
+      
+      
+    </nav>
+
   )
 }
 
