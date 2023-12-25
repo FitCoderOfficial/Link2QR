aad2492794b201e677ec40cdc73d1d2a4b61f6bd
Shin Saebyeok <toqur1219@naver.com>
2023-12-25 16:19:56 +0900

    core:Renewer All

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next'
+import '../../styles/globals.css'
+import Nav from '../../components/Nav'
+
+export const metadata: Metadata = {
+  title: 'My QR Code',
+  description: 'QR Code Generator',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="ko">
+      <body>
+        <div className=''>
+          <div className='gradient' />
+        </div>
+
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </body>
+    </html>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
         notosans: ['Noto Sans KR', 'sans-serif'],
         satoshi: ['Satoshi', 'sans-serif'],
         inter: ['Inter', 'sans-serif'],
@@ -18,5 +18,6 @@ module.exports = {
       }
     },
   },
-  plugins: [],
 }
+
+export default config
